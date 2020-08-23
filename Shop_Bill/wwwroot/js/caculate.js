0177d6d719e0ecebf6995039e03b90da69a23a75
anhnghia47 <sam@google.com>
2020-08-23 11:47:51 +0700

    Upgrade UI and Js

---
+++ Shop_Bill/wwwroot/js/caculate.js
@@ -0,0 +1,38 @@
+﻿// Biến đếm 
+
+$(document).ready(function (e) {
+    caculate = new Caculate();
+});
+
+class Caculate {
+    constructor() {
+        this.initEvent();
+    };
+
+    initEvent() {
+        $("#head-menu, #head-menu-text").click(this.clickMenu);
+        $("#table-main tbody tr").click(clickOnRow);
+        $("#table-main tbody tr").dblclick(dblClickOnRow);
+        $("#option-mode li").click(clickOption);
+    }
+
+    // Hiện,Ẩn menu 
+    clickMenu(e) {
+        var showMainLeft = $(".main-left").toggle().is(":visible");
+        if (showMainLeft)
+            $(".main-right").css("width", "calc(100% - 200px)");
+        else
+            $(".main-right").css("width", "100%");
+    }
+};
+
+// Xử lí click 1 hoặc nhiều hàng
+function clickOnRow(e) {
+    $(this).addClass("row-clicked");
+}
+
+// Xử lí double click 1 hàng
+function dblClickOnRow(e) {
+    $(this).remove();
+}
+
---
+++ Shop_Bill/wwwroot/js/common.js
@@ -0,0 +1,19 @@
+﻿// Xử lí khi click tùy mục
+function clickOption(e) {
+    var option = $(this).attr("op-mode");
+    if (option == 1) {
+        $("#main-title").text("Danh mục tính tiền");
+        $(".main-caculate").css("display", "");
+        $(".main-caculate").siblings().css("display", "none");
+    }
+    if (option == 2) {
+        $("#main-title").text("Danh mục sản phẩm");
+        $(".main-product").siblings().css("display", "none");
+        $(".main-product").css("display", "");
+    }
+    if (option == 3) {
+        $("#main-title").text("Danh mục hóa đơn");
+        $(".main-bill").siblings().css("display", "none");
+        $(".main-bill").css("display", "");
+    }
+}
